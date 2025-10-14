8ac207c7cbed5807efca90f778d5b6a0635baf88
MyListon <myliston@gmail.com>
2025-10-14 11:46:49 -0300

    :sparkles: Finalização do CRUD do Recurso Postagem

--- src/main/java/com/generattion/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generattion/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,13 @@
 package com.generattion.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generattion.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	// SELECT * FROM tb_postagens WHERE titulo LIKE "%?%";
 }
